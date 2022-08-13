bf15c35eeb2f19475d2be5c8d0b77ca0c670d8a5
SmartCom80 <smartcom.ter@gmail.com>
2022-08-14 00:49:12 +0300

    added solve Task__03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,73 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = new FormData();
+
+form.addEventListener('input', throttle(onFeedbackInput, 500));
+form.addEventListener('submit', onFeedbackSubmit);
+
+// 1. При загрузке/перезагрузке страницы, считываем наличие записи в localStorage по ключу STORAGE_KEY
+onReadStorage();
+
+// 2. Функция чтения данных из LocalStorage
+function onReadStorage() {
+  const savedForm = localStorage.getItem(STORAGE_KEY);
+  //   console.log('savedForm :>> ', localStorage.getItem(STORAGE_KEY));
+  if (savedForm) {
+    const target = onCheckData(savedForm);
+
+    formData.set('email', target.email);
+    formData.set('message', target.message);
+    // 2. Если в хранилище есть данные, вызываем функцию для перезаписи данных в поля формы
+    onReWriteFormField();
+  }
+}
+
+// 3. Функция проверки данных, если это строка то парсит данные в форму,
+//если объект, то переводит в строку для записи в LocalStorage
+function onCheckData(subject) {
+  if (typeof subject === 'string') {
+    return JSON.parse(subject);
+  } else {
+    return JSON.stringify(Object.fromEntries(formData.entries()));
+  }
+}
+
+// 4. Вызываем функцию записи в поля формы
+function onReWriteFormField() {
+  //   console.log('form :>> ', formData.get('email'));
+  form.email.value = formData.get('email');
+  form.message.value = formData.get('message');
+}
+
+// 5. Функция записи данных формы в LocalStorage
+function onWriteStorage() {
+  localStorage.setItem(STORAGE_KEY, onCheckData(formData));
+}
+
+// 6. Колбек-функция для получения данных из формы
+function onFeedbackInput(event) {
+  const evt = event.target;
+
+  //   console.log('evt.name :>> ', evt.name);
+  //   console.log('evt.value :>> ', evt.value);
+
+  formData.set(evt.name, evt.value);
+
+  onWriteStorage();
+  return formData;
+}
+
+// 7. Колбек-функция для отправки данных формы
+function onFeedbackSubmit(event) {
+  event.preventDefault();
+  if (formData.get('email') === 'null' || formData.get('password') === 'null') {
+    window.alert('Please fill in all the fields!');
+    return;
+  }
+  console.log('email: ', formData.get('email'));
+  console.log('message: ', formData.get('message'));
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
